42ec0615b3c12f5f68f62372aeb328fb1d6a688d
Nwosu Victor <nwosuvictor54@gmail.com>
2023-01-24 22:18:33 +0400

    exported as default

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export async function asyncUploadUser() {
+export default async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
